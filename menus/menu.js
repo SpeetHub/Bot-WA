ca7eda9ff050059e8a6fb8a3c2f6d1d04c6d2876
$ P E E T <103465768+SpeetHub@users.noreply.github.com>
2022-06-12 02:31:50 -0300

    Add files via upload

---
+++ menus/menu.js
@@ -0,0 +1,130 @@
+const menu = (prefix, NomeDoBot) => {
+  
+// NÃO APAGUE ESSE ${NickDono} nem 
+//${numerodn} nem ${NomeDoBot} nem ${prefix} só se quiser apagar completo, não coloque nada ${dentro assim} ISSO SÃO DEFINIÇÕES QUE ESTÁ PUXANDO DO settings.json, da pasta dono, só pode altera a base de tudo, menos as definições, só se quiser apagar a definição completa.  
+  
+return `​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​
+金────────㑹────────金
+       ${NomeDoBot}             
+金────────㑹────────金
+                                             
+╓▱▱▱▱▱▱⛩️▱▱▱▱▱▱╖
+   ⾕──⟬ 𝐁𝐄𝐌-𝐕𝐈𝐍𝐃𝚯(𝐀)⟭──⾕
+╙▱▱▱▱▱▱ 〄 ▱▱▱▱▱▱╜
+
+ ⚏⚏⚏⚏⚏ ⛛ ⚏平⚏ ⛛ ⚏⚏⚏⚏⚏
+               𝐌𝐄𝐍𝐔 𝐑𝐀𝐏𝐈𝐃𝚯          
+ 金────────㑹────────金
+
+║𖣴⋗${prefix}menu2
+║𖣴⋗${prefix}Menudono
+║𖣴⋗${prefix}Efeitosimg
+║𖣴⋗${prefix}Menuadm
+║𖣴⋗${prefix}Menupremium
+║𖣴⋗${prefix}Logos
+║𖣴⋗${prefix}Brincadeiras
+║𖣴⋗${prefix}Alteradores
+
+ ⚏⚏⚏⚏⚏ ⛛ ⚏平⚏ ⛛ ⚏⚏⚏⚏⚏
+           𝐌𝐄𝐍𝐔 𝐌𝐄𝐌𝐁𝐑𝚯𝐒       
+ 金────────㑹────────金
+
+║𖣴⋗${prefix}Infobot
+║𖣴⋗${prefix}Idiomas 
+║𖣴⋗${prefix}Bug (QUESTIONE) 
+║𖣴⋗${prefix}Sugestao (DICA) 
+║𖣴⋗${prefix}Avalie (O-QUAO-BOM) 
+
+ ⚏⚏⚏⚏⚏ ⛛ ⚏平⚏ ⛛ ⚏⚏⚏⚏⚏
+                 𝐌𝐄𝐍𝐔 𝐃𝚯𝐍𝚯           
+ 金────────㑹────────金
+
+║𖣴⋗${prefix}Fotomenu (MARCAR-IMG) 
+║𖣴⋗${prefix}Gifmenu (MARCAR) 
+║𖣴⋗${prefix}InfoBemvindo
+║𖣴⋗${prefix}Infopalavrão
+║𖣴⋗${prefix}Infolistanegra
+║𖣴⋗${prefix}Infobancarac
+║𖣴⋗${prefix}Infovotação
+║𖣴⋗${prefix}Infocontador
+║𖣴⋗${prefix}Infosorteio
+
+ ⚏⚏⚏⚏⚏ ⛛ ⚏平⚏ ⛛ ⚏⚏⚏⚏⚏
+        𝐏𝐄𝐒𝐐𝐔𝐈𝐒𝐀𝐑│𝐁𝐀𝐈𝐗𝐀𝐑      
+ 金────────㑹────────金
+ 
+║𖣴⋗${prefix}Play (NOME) 
+║𖣴⋗${prefix}Playmp4 (NOME) 
+║𖣴⋗${prefix}Ytsearch (NOME) 
+║𖣴⋗${prefix}gimage (NOME)
+║𖣴⋗${prefix}Pinterest (NOME)
+║𖣴⋗${prefix}print (LINK)
+║𖣴⋗${prefix}Ytmp4 (LINK) 
+║𖣴⋗${prefix}Ytmp3 (LINK) 
+║𖣴⋗${prefix}Tiktok (LINK) 
+║𖣴⋗${prefix}Instadw (LINK) 
+║𖣴⋗${prefix}Twitter (LINK) 
+║𖣴⋗${prefix}Imgpralink (MARCAR)
+║𖣴⋗${prefix}Videopralink (MARCAR-V) 
+
+ ⚏⚏⚏⚏⚏ ⛛ ⚏平⚏ ⛛ ⚏⚏⚏⚏⚏
+                      𝐉𝚯𝐆𝚯𝐒                  
+ 金────────㑹────────金
+
+║𖣴⋗${prefix}Ppt (PEDRA/PAPEL/TESOURA) 
+║𖣴⋗${prefix}Jogodavelha (@MARCAR) 
+║𖣴⋗${prefix}Ttt (JOGO-DA-VELHA) 
+║𖣴⋗${prefix}Cassino
+║𖣴⋗${prefix}Quizanime 1 / 0
+║𖣴⋗${prefix}Quizanimais 1 / 0
+║𖣴⋗${prefix}Anagrama 1 / 0
+
+ ⚏⚏⚏⚏⚏ ⛛ ⚏平⚏ ⛛ ⚏⚏⚏⚏⚏
+                 𝐅𝐈𝐆𝐔𝐑𝐈𝐍𝐇𝐀𝐒            
+ 金────────㑹────────金
+
+║𖣴⋗${prefix}Attp (TEXTO)
+║𖣴⋗${prefix}Ttp (TEXTO)
+║𖣴⋗${prefix}Fsticker (MARCAR-FOTO)
+║𖣴⋗${prefix}Sticker (MARCAR-FOTO)
+║𖣴⋗${prefix}Toimg (MARCAR-FIGU)
+║𖣴⋗${prefix}Togif (MARCAR-FIGU)
+║𖣴⋗${prefix}Roubar (TEXT/TEXT)
+║𖣴⋗${prefix}packfigu
+
+ ⚏⚏⚏⚏⚏ ⛛ ⚏平⚏ ⛛ ⚏⚏⚏⚏⚏
+    𝐈𝐍𝐅𝚯𝐑𝐌𝐀𝐂𝚯𝐄𝐒 𝐁𝐀𝐒𝐈𝐂𝐀𝐒 
+ 金────────㑹────────金
+
+║𖣴⋗${prefix}Ping (VELO) 
+║𖣴⋗${prefix}Atividade
+║𖣴⋗${prefix}Rankativo
+║𖣴⋗${prefix}Checkativo (@MARCAR)
+║𖣴⋗${prefix}Ranklevel (DE-TODOS) 
+
+ ⚏⚏⚏⚏⚏ ⛛ ⚏平⚏ ⛛ ⚏⚏⚏⚏⚏
+       𝐂𝚯𝐌𝐀𝐍𝐃𝚯𝐒 𝐁𝐀𝐒𝐈𝐂𝚯𝐒     
+ 金────────㑹────────金
+ 
+║𖣴⋗${prefix}Gtts (LINGUAGEM + TEXTO)
+║𖣴⋗${prefix}Traduzir Hello 
+║𖣴⋗${prefix}Tagme 
+║𖣴⋗${prefix}Emoji
+║𖣴⋗${prefix}Emojimix
+║𖣴⋗${prefix}Tabela (LETRAS) 
+║𖣴⋗${prefix}Conselhobiblico
+║𖣴⋗${prefix}Simi (FALE-ALGO)  
+║𖣴⋗${prefix}Perfil
+║𖣴⋗${prefix}Frases
+║𖣴⋗${prefix}Calcular (1 + 1)
+║𖣴⋗${prefix}Fazernick (NICK)
+║𖣴⋗${prefix}titã
+金──────────㑹──────────金
+
+`
+}
+
+exports.menu = menu
+
+// NÃO APAGUE ESSE ${NickDono} nem 
+//${numerodn} nem ${NomeDoBot} nem ${prefix} só se quiser apagar completo, não coloque nada ${dentro assim} ISSO SÃO DEFINIÇÕES QUE ESTÁ PUXANDO DO settings.json, da pasta dono, só pode altera a base de tudo, menos as definições, só se quiser apagar a definição completa.
